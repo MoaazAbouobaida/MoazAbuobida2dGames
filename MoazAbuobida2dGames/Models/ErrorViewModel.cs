3b8b04c4765c23aeba930cc5ff38557396895ddf
MoaazLocalServer <MoaazLocalServer@MOAAZLOCALSERVE>
2024-05-18 08:29:54 +0300

    Add project files.

---
+++ MoazAbuobida2dGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoazAbuobida2dGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
